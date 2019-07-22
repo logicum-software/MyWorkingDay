aea20572d674a3ff34348fa4475e23576e18525a
Markus Gärtner <gaertner.m@live.de>
2019-07-22 09:41:46 +0200

    20190722 09:41

---
+++ MyWorkingDay/Aufgabe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWorkingDay
+{
+    [Serializable]
+    class Aufgabe
+    {
+        private String strName { get; set; }
+        private DateTime dtPlannedStart { get; set; }
+        private DateTime dtPlannedEnd { get; set; }
+        private DateTime dtStart { get; set; }
+        private DateTime dtEnd { get; set; }
+
+        public Aufgabe()
+        {
+            strName = "";
+            dtPlannedStart = DateTime.Now;
+            dtPlannedEnd = DateTime.Now.AddDays(7);
+        }
+
+        public Aufgabe(String name, DateTime plannedStart, DateTime plannedEnd)
+        {
+            strName = name;
+            dtPlannedStart = plannedStart;
+            dtPlannedEnd = plannedEnd;
+        }
+    }
+}
