79c88d462cd4c57d9ce01af39768f274ec50db41
Markus Gärtner <gaertner.brilon@gmail.com>
2019-10-02 09:47:58 +0200

    20191002 09:47

--- MyWorkingDay/TaskSelection.xaml.cs
+++ MyWorkingDay/TaskSelection.xaml.cs
@@ -19,14 +19,50 @@ namespace MyWorkingDay
     /// </summary>
     public partial class TaskSelection : Window
     {
+        private List<Aufgabe> tmpAufgaben { get; set; }
+
         public TaskSelection()
         {
             InitializeComponent();
+
+            listBoxSelectTask.ItemsSource = tmpAufgaben;
+        }
+
+        internal void setAufgaben(List<Aufgabe> tmpList)
+        {
+            tmpAufgaben = tmpList;
         }
 
         private void ButtonCancel_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void ButtonTaskNew_Click(object sender, RoutedEventArgs e)
+        {
+            NewTask dlgNewTask = new NewTask();
+
+            dlgNewTask.ShowDialog();
+
+            if (dlgNewTask.DialogResult.HasValue && dlgNewTask.DialogResult.Value == true)
+            {
+                foreach (Aufgabe item in tmpAufgaben)
+                {
+                    if (String.Compare(item.strName, dlgNewTask.textBoxName.Text, true) > -1 &&
+                        String.Compare(item.strName, dlgNewTask.textBoxName.Text, true) < 1)
+                    {
+                        MessageBox.Show("Eine Aufgabe mit dem Namen " + dlgNewTask.textBoxName.Text +
+                            " existiert bereits.\n\nBitte wählen Sie einen anderen Namen.", "Aufgabe vorhanden", MessageBoxButton.OK);
+                        return;
+                    }
+                }
+                /*appData.Aufgaben.Add(new Aufgabe(dlgNewTask.textBoxName.Text, dlgNewTask.textBoxDescription.Text,
+                    dlgNewTask.datePickerStart.DisplayDate, dlgNewTask.datePickerEnd.DisplayDate, (Boolean)dlgNewTask.checkBox.IsChecked));
+                saveData();
+                listBoxTasks.Items.Refresh();*/
+                //saveData();
+                MessageBox.Show("Die Aufgabe wurde gespeichert", "Aufgabe gespeichert", MessageBoxButton.OK);
+            }
         }
     }
 }
